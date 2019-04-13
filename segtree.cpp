a4754c7091625810de227eb3329897b52cbdf5b2
Kuan Wei Heng <kuanwh1996@gmail.com>
2019-04-13 20:35:12 +0800

    Preserve range query bounds information in segtree

--- segtree.cpp
+++ segtree.cpp
@@ -44,14 +44,14 @@ struct segtree {
     void update(int ql, int qr, ll val) { update(ql, qr, val, 1, 0, n-1); }
     void update(int ql, int qr, ll val, int v, int l, int r) {
         push(v, l, r);
-        if (ql > qr) return;
-        if (ql == l && qr == r) {
+        if (l > qr || r < ql) return;
+        if (ql <= l && r <= qr) {
             lazy[v] = val;
             push(v, l, r);
         } else {
             int m = (l+r)/2;
-            update(ql, min(qr, m), val, v*2, l, m);
-            update(max(ql, m+1), qr, val, v*2+1, m+1, r);
+            update(ql, qr, val, v*2, l, m);
+            update(ql, qr, val, v*2+1, m+1, r);
             t[v] = combine(t[v*2], t[v*2+1]);
         }
     }
@@ -59,9 +59,18 @@ struct segtree {
     node query(int ql, int qr) { return query(ql, qr, 1, 0, n-1); }
     node query(int ql, int qr, int v, int l, int r) {
         push(v, l, r);
-        if (ql > qr) return {0}; // identity
-        if (ql == l && r == qr) return t[v];
+        if (l > qr || r < ql) return {0}; // identity
+        if (ql <= l && r <= qr) return t[v];
         int m = (l+r)/2;
-        return combine(query(ql, min(qr, m), v*2, l, m), query(max(ql, m+1), qr, v*2+1, m+1, r));
+        return combine(query(ql, qr, v*2, l, m), query(ql, qr, v*2+1, m+1, r));
+    }
+
+    node query(int qx) { return query(qx, 1, 0, n-1); }
+    node query(int qx, int v, int l, int r) {
+        push(v, l, r);
+        if (l == r) return t[v];
+        int m = (l+r)/2;
+        if (qx <= m) return query(qx, v*2, l, m);
+        else         return query(qx, v*2+1, m+1, r);
     }
 };
